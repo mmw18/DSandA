076fd402b993ebfbf05ba1e20399f3aeaa924a7b
Megan Wright <meganmwright2018@gmail.com>
2024-12-15 11:30:07 -0600

    Connection to GH, module 1

---
+++ CR1Bag.java
@@ -0,0 +1,10 @@
+/**
+ * Module 1 Critical Thinking
+ * Megan M. Wright
+ * Colorado State Univeristy Global
+ * CSC400-1: Data Structures and Algorithms
+ * Professor Pensado, Herbert
+ * December 15th, 2024
+ */
+
+ 
