a9dd7bed65799db0da22c7b736bacd2959e031b6
Megan Wright <meganmwright2018@gmail.com>
2024-12-15 12:09:46 -0600

    Pseudocode added, prior to testing

--- CR1Bag.java
+++ CR1Bag.java
@@ -85,3 +85,26 @@ public class CR1Bag<T> {
         System.out.println("Count of 'Chocolate Chip Cookies': " + bag.count("Chocolate Chip Cookies"));
     }
 }
+
+/*
+ * PSEUDOCODE
+ * 
+ * START
+ * Bag Class: This will be holding all the items (in this case I chose cookies of different flavors), and how many of each there are.
+ *   Use a HashMap since this would be best for cases of storing items as keys and their occurances.
+ * 
+ * Methods needed: 
+ * - Adding an item to the bag: If the cookie flavor already exists in the bag then add 1 to it's count. If it does not exist within the bag then add it with a count of 1.
+ * - Removing an item from the bag: If the cookie flavor IS in the bag drop it's count by 1. If the count of the flavor hits 0, then completely remove this cookie from the bag. 
+ *     If the cookie entered doesn't exist in the bag when trying to be removed, print a message to the user to let them know
+ * - Checking if an item exists in the bag at all: Review bag items to see if the cookie in question does exist or not. Provide this answer in a true/false
+ * - Counting of a specific item: Check how many cookies of the flavor in question do exist in the bag currently. If the flavor doesn't exist at all the program should print 0.
+ * - Printing the contents of the bag to the user: Show all cookie flavors in the bag and how many of each there are
+ * 
+ * Main Method/Program: 
+ *  - Initialize an empty bag to hold all the cookies
+ *  - Add cookies of differing flavors, with at least one duplicate
+ *  - Show everything that's in the bag since initial additions
+ *  - Testing methods to see if a flavor exists, how many of a flavor are in the bag, and needing to remove from the bag.
+ * END
+ */
